1e2fa211cffc9f5216f18cd761a9a9a84d4b98d3
Tania <tania@efaber.net>
2015-04-30 13:34:35 +0200

    Upgrade backend to spree-3-0-stable with bootstrap

--- app/overrides/decorate_admin_products_index.rb
+++ app/overrides/decorate_admin_products_index.rb
@@ -15,6 +15,6 @@ Deface::Override.new(
 Deface::Override.new(
   :virtual_path => "spree/admin/products/index",
   :name => "multi_domain_admin_products_index_search",
-  :insert_top => "[data-hook='admin_products_index_search']",
+  :insert_before => "[data-hook='admin_products_index_search']",
   :partial => "spree/admin/products/index_search_fields",
   :disabled => false)
