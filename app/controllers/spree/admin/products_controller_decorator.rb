51e893c0187cf26bbf72a36d99f6623bb0b68a41
Tania <tania@efaber.net>
2015-05-13 12:28:57 +0200

    Do not nullify store_ids on before update

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,9 +1,9 @@
 Spree::Admin::ProductsController.class_eval do
-  update.before :set_stores
+  # update.before :set_stores
 
-  private
-  def set_stores
-    @product.store_ids = nil unless params[:product].key? :store_ids
-  end
+  # private
+  # def set_stores
+  #   @product.store_ids = nil unless params[:product].key? :store_ids
+  # end
 
 end
